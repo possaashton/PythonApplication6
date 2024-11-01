db2c2990e5763f4faf126cd09215522e6c76a68d
Ashton_Lenovo <possaashton@gmail.com>
2024-11-01 17:10:15 +0000

    Add project files.

---
+++ PythonApplication6.py
@@ -0,0 +1,10 @@
+
+# Python program to convert .tsv file to .csv file
+# importing pandas library
+import pandas as pd
+#import numpy as np
+
+DIR = r"D:\DublinBusinessSchool\Semister_3_Dissertation\Applied_Research_Project\DataSet"
+DATASET = pd.read_csv(DIR + r"\raw_test.csv")
+
+print(DATASET.head)
